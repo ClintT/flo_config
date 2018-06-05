8ed7cd75fd786fe32ad1f41fa297b4a1d0341817
ClintT <clintthebus@gmail.com>
2018-06-05 15:39:49 +0200

    Update common_mini_tablet_wifionly.mk

--- common_mini_tablet_wifionly.mk
+++ common_mini_tablet_wifionly.mk
@@ -1,5 +1,5 @@
-# Inherit mini common Bootleg stuff
-$(call inherit-product, vendor/bootleggers/config/common_mini.mk)
+# Inherit mini common cardinal stuff
+$(call inherit-product, vendor/cardinal/common_mini.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
