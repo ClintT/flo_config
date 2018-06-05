ab4056efc17d69645c69015e1ddab129fd0ca115
ClintT <clintthebus@gmail.com>
2018-06-05 17:19:56 +0200

    Update common_mini_tablet_wifionly.mk

--- common_mini_tablet_wifionly.mk
+++ common_mini_tablet_wifionly.mk
@@ -1,5 +1,5 @@
 # Inherit mini common cardinal stuff
-$(call inherit-product, vendor/cardinal/common_mini.mk)
+$(call inherit-product, vendor/cardinal/config/common_mini.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
