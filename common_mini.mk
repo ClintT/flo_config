12d14154486b1fc868d607cfd9ae5fcc48786e22
ClintT <clintthebus@gmail.com>
2018-06-12 07:36:48 +0200

    Update common_mini.mk

--- common_mini.mk
+++ common_mini.mk
@@ -1,4 +1,4 @@
-# Inherit mini common bootleg stuff
-$(call inherit-product, vendor/bootleggers/config/common.mk)
+# Inherit mini common dot stuff
+$(call inherit-product, vendor/dot/config/common.mk)
 
 PRODUCT_SIZE := mini
